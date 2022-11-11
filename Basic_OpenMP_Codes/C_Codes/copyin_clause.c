7a03a449d73fe7aeb0da1176027fd48d43536f78
Kuldeep Tolia <toliakuldeep1998@gmail.com>
2022-11-11 16:42:18 +0530

    Add files via upload

---
+++ Basic_OpenMP_Codes/C_Codes/copyin_clause.c
@@ -0,0 +1,28 @@
+// OpenMP program to demonstrate the copyin clause
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	int id;
+	static int x;
+	
+	#pragma omp threadprivate(x)
+	#pragma omp parallel private(id)
+	{
+		id = omp_get_thread_num();
+		x = x + id;
+		printf("private x: %d from thread: %d \n", x, id);
+	}
+	
+	#pragma omp parallel copyin(x)
+	{}
+	
+	#pragma omp parallel private(id)
+	{
+		id = omp_get_thread_num();
+		x = x + 10;
+		printf("2nd parallel x: %d from thread: %d \n", x, id);
+	}
+}
